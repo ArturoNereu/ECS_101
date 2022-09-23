f1603a1b0a17231078960c530b7dc68b5c132fc2
Arturo Nereu <hdgam3r@gmail.com>
2022-09-22 23:28:09 -0500

    Added more entities to the project

--- ECS_Drone/Assets/Scripts/Hybrid/DroneMoveSystem.cs
+++ ECS_Drone/Assets/Scripts/Hybrid/DroneMoveSystem.cs
@@ -2,21 +2,26 @@
 using Unity.Entities;
 using Unity.Transforms;
 using Unity.Mathematics;
+using Random = UnityEngine.Random;
 
-// Here we work on the system. Note that we are not extending MonoBehaviour, but ComponentSystem so we won't be able to attach this
-// script to an object, the System (DroneMoveSystem) will execute over all the objects that meet a criteria (query). That criteria
-// is defined as a parameter list on the Foreach method. So, this system will act on all objects that contain a Transform and the DroneMoveComponent components.
 public class DroneMoveSystem : ComponentSystem
-{   
-    // The OnUpdate is similar to our MonoBehaviour Update() however, instead of calling it on a per object basis each frame,
-    // we call it once every frame and then iterate over all the entities that match the criteria.
+{
     protected override void OnUpdate()
     {
         float dt = (float)Time.ElapsedTime;
 
         Entities.ForEach((Entity entity, ref Translation translation, ref DroneMoveComponent droneMoveComponent) =>
         {
-            translation.Value = new Vector3(0, math.sin(dt * droneMoveComponent.speed) * 0.5f, 0);
+            translation.Value.y = math.sin(dt * droneMoveComponent.speed) * 0.5f;
         });
     }
+    
+    /*protected override void OnStartRunning()
+    {
+        Entities.ForEach((Entity entity, ref Translation translation, ref DroneMoveComponent droneMoveComponent) =>
+        {
+            droneMoveComponent.speed = Random.Range(1.5f, 3.5f);
+            translation.Value = new Vector3(Random.Range(-20.0f, 20.0f), 0.308f, Random.Range(-20.0f, 20.0f));
+        });
+    }*/
 }
